2b4523775844f6587453a6c3633003136f5c52b7
Vital4uk <vital4uk777@gmail.com>
2023-01-06 18:52:55 +0200

    mini program with CLASS from Ravesti

--- prog6_6.cpp
+++ prog6_6.cpp
@@ -3,23 +3,23 @@
 class DateClass
 {
 public:
-    int m_day;
-    int m_month;
-    int m_year;
+    int m_day;
+    int m_month;
+    int m_year;
  
-    void print()
-    {
-        std::cout << m_day << "/" << m_month << "/" << m_year;
-    }
+    void print()
+    {
+        std::cout << m_day << "/" << m_month << "/" << m_year;  
+    }
 };
  
 int main()
 {
-    DateClass today { 12, 11, 2018 };
- 
-    today.m_day = 18; // используем оператор выбора членов для выбора переменной-члена m_day объекта today класса DateClass
-    today.print(); // используем оператор выбора членов для вызова метода print() объекта today класса DateClass
- 
-    return 0;
+    DateClass today { 12, 11, 2018 };
+
+    today.m_day = 18; // используем оператор выбора членов для выбора переменной-члена m_day объекта today класса DateClass
+    today.print(); // используем оператор выбора членов для вызова метода print() объекта today класса DateClass
+                   
+    return 0;
 
 }
